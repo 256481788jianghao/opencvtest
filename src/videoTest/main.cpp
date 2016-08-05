5b633d82a28ab68104f77abd3b370ba0d1b8a47c
jj <256481788@163.com>
2016-08-05 17:01:29 +0800

    add videotest

---
+++ src/videoTest/main.cpp
@@ -0,0 +1,13 @@
+#include "opencv2/opencv.hpp"
+using namespace cv;
+
+int main(){
+    VideoCapture capture("1.avi");
+    while(1){
+        Mat frame;
+        capture >> frame;
+        imshow("xxxx",frame);
+        waitKey(30);
+    }
+    return 0;
+}
